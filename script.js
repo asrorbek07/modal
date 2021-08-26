a0523d3410223221d65f99ec3f9bdf783dcb161d
asrorbek07 <asrorobidjonov2000@gmail.com>
2021-08-26 16:37:52 +0500

    finish

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+const elShowModals =document.querySelectorAll(".show-modal");
+const elModal =document.querySelector(".modal");
+const elCloseModal = document.querySelector(".close-modal");
+const elOverlay = document.querySelector(".overlay");
+
+function openModal(){
+    elModal.classList.remove("hidden");
+    elOverlay.classList.remove("hidden");
+}
+function closeModal(){
+    elModal.classList.add("hidden");
+    elOverlay.classList.add("hidden");
+}
+
+elShowModals.forEach(elShowModal => { 
+    elShowModal.addEventListener("click",openModal);
+});
+
+elCloseModal.addEventListener("click", closeModal);
+elOverlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function(evt){
+    if(evt.key==='Escape' && !elModal.classList.contains('hidden')){
+        closeModal();
+    }
+});
